748a57e23d7975a2f51e2ec1455bc34dac9b8a24
naisthorpe <aisthorpe@gmail.com>
2021-04-24 17:39:48 -0400

    Build index in models folder

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,6 +10,7 @@ router.get('/', async (req, res) => {
     const categoryData = await Category.findAll({
       include: [{ model: Product }]
     });
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
